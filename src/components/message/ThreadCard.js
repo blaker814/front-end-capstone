29dae8f0bc08f6721e6f3bffbd65080f69e8fde4
Ricky Blake <rab0196@yahoo.com>
2020-10-20 16:02:27 -0500

    removed last message from thread

--- src/components/message/ThreadCard.js
+++ src/components/message/ThreadCard.js
@@ -4,15 +4,7 @@ import { MessageContext } from "./MessageProvider"
 import { Link } from "react-router-dom"
 
 export const ThreadCard = ({ friend }) => {
-    const { messages, getMessages, removeMessage } = useContext(MessageContext)
-
-    useEffect(() => {
-        getMessages()
-    }, [])
-
-    const threadMessages = messages.filter(message => {
-        return message.threadOneId === friend.id || message.threadTwoId === friend.id
-    })
+    const { removeMessage } = useContext(MessageContext)
 
     return (
         <section className="thread">
@@ -21,7 +13,6 @@ export const ThreadCard = ({ friend }) => {
                     {friend.user.username}
                 </Link>
             </h3>
-            <p>{threadMessages[0]?.message}</p>
             <Button icon negative type="button" style={{ marginLeft: 10 }} onClick={() => {
                 removeMessage(friend.id)
             }}><Icon name="trash" /></Button>
